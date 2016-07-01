7676781979ad00514fb03635e0ea055d92b1d60a
Samuel Williamson <Samuel Williamson>
2016-07-01 13:42:37 +0100

    Added iBrateMe to Git

---
+++ iBrateMeWin10/MainPage.xaml.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.Phone.Devices.Notification;
+using Windows.Devices;
+using Microsoft;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace iBrateMeWin10
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        bool ToggleOn;
+        private DispatcherTimer timer;
+        double VibrationDelay;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            this.NavigationCacheMode = NavigationCacheMode.Required;
+        }
+
+        /// <summary>
+        /// Invoked when this page is about to be displayed in a Frame.
+        /// </summary>
+        /// <param name="e">Event data that describes how this page was reached.
+        /// This parameter is typically used to configure the page.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            // TODO: Prepare page for display here.
+
+            // TODO: If your application contains multiple pages, ensure that you are
+            // handling the hardware Back button by registering for the
+            // Windows.Phone.UI.Input.HardwareButtons.BackPressed event.
+            // If you are using the NavigationHelper provided by some templates,
+            // this event is handled for you.
+        }
+
+        //private void frequencyValueTextBlock_SelectionChanged(object sender, RoutedEventArgs e)
+        //{
+        //frequencyValueTextBlock.Text = frequencySlider.TickFrequency.ToString;
+        //}
+
+        void iBrateMeToggleButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void iBrateMeToggleButton_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            ToggleOn = !ToggleOn;
+
+            if (ToggleOn)
+            {
+                StartVibrateStoryboard.Begin();
+
+                //VibrationDevice testVibrationDevice = VibrationDevice.GetDefault();
+                //testVibrationDevice.Vibrate(TimeSpan.FromMilliseconds(1000));
+
+                timer = new DispatcherTimer();
+                timer.Tick += timer_Tick;
+                timer.Interval = TimeSpan.FromMilliseconds(VibrationDelay);
+
+                timer.Start();
+
+            }
+            else
+            {
+                StartVibrateStoryboard.Stop();
+                timer.Stop();
+            }
+
+        }
+
+        void timer_Tick(object sender, object e)
+        {
+
+            timer.Start();
+            timer.Interval = TimeSpan.FromMilliseconds(VibrationDelay);
+        }
+
+        private void frequencySlider_DataContextChanged(FrameworkElement sender, DataContextChangedEventArgs args)
+        {
+            VibrationDelay = frequencySlider.Value;
+        }
+    }
+}
